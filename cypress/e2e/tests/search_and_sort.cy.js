d3fc25e645451883b4c62264eca2294bcb2c2763
artem1911 <artem.tarasov.11@protonmail.ch>
2022-11-05 16:38:24 +0100

    test: update imports and methods, add custom commands

--- cypress/e2e/tests/search_and_sort.cy.js
+++ cypress/e2e/tests/search_and_sort.cy.js
@@ -1,28 +1,25 @@
 /// <reference types="cypress"/>
 
 import { navigation } from "../../objects/navigation";
-import { searchbar } from "../../objects/searchBar";
-import { buttons } from "../../objects/buttons";
 import { sortBar } from "../../objects/sortBar";
 import { productCard } from "../../objects/productCard";
 import { utilites } from "../../objects/utilites";
 
 describe('Product verification after searching and sorting', () => {
 
-    before(() => {
-        navigation.mainPage();
-        buttons.cookiesBtn().click();
-      })
-
-      beforeEach(() => {
-        cy.preserveCookieOnce('connect.session');
+    beforeEach(() => {
+        navigation.mainPage(); 
+        cy.search('trampolin'); 
       })
       
-    it('Search by product type, sort by price of products', () => {
-        searchbar.inputSearch().type('trampolin');
-        searchbar.searchFormBtn().click();
-        sortBar.sortSelectMenu(1).select('priceDesc');
-        utilites.sortFunc(5, productCard.productPrice);
-        
+    it('Search by product type of trampolin', () => {    
+        navigation.getUrl('/suche/trampolin/');
+        navigation.getTitle('trampolin | myToys');
+        utilites.sortFunc(5, productCard.productName);  
     })
+
+    it('Verify sort by Highest price', () => {      
+      sortBar.sortSelectMenu(1).select('priceDesc');
+      utilites.sortFunc(5, productCard.productPrice);    
+  })
 }) 
