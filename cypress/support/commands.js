d0c3cb88ac9732709cdc99d71d815233ca637ba7
artem1911 <artem.tarasov.11@protonmail.ch>
2022-11-05 16:33:27 +0100

    test: add custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -7,7 +7,8 @@
 // commands please read more here:
 // https://on.cypress.io/custom-commands
 // ***********************************************
-//
+import { searchbar } from './../objects/searchBar';
+import { filters } from "./../objects/filter";
 //
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
@@ -21,5 +22,16 @@
 // Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('search', (text) => {
+    searchbar.inputSearch().type(text);
+    searchbar.searchFormBtn().click({force: true});
+})
+
+Cypress.Commands.add('priceFilter',(startValue, endValue) => { 
+    filters.priceFilterBtn(0).click();
+    filters.pricerangeSlider_valueStart(0).clear().type(startValue);
+    filters.pricerangeSlider_valueEnd(0).clear().type(endValue);
+    filters.pricerangeSubmit(0).click();
+    filters.priceSubmit(0).click();
+})
